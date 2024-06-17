2a5bbe1c9099e86216542861807a321aea5cf672
William Sampaio <56176702+WilliamSampaio@users.noreply.github.com>
2024-06-17 18:08:07 -0400

    update

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <ncurses.h>
 #include <iostream>
 #include <vector>
+#include <unistd.h>
+
+enum Direction
+{
+    Left,
+    Up,
+    Right,
+    Down
+};
 
 struct UnitType
 {
@@ -80,7 +89,10 @@ protected:
     UnitType food;
     char food_char = 'O';
 
-    int delay = 1000;
+    int delay = 100000;
+
+    Direction direction = Left;
+    bool tail_stop = false;
 
     Plane *plane;
 
@@ -98,8 +110,6 @@ public:
 
         plane = new Plane(max_width, max_height);
 
-        plane->draw();
-
         srand(time(NULL));
         insert_food();
 
@@ -113,6 +123,8 @@ public:
             move(snake[i].s_y, snake[i].s_x);
             addch(snake_char);
         }
+
+        plane->draw();
 
         move(food.s_y, food.s_x);
         addch(food_char);
@@ -151,10 +163,87 @@ public:
             break;
         }
     }
+
+    void move_snake()
+    {
+        int tmp = getch();
+
+        switch (tmp)
+        {
+        case KEY_LEFT:
+            if (direction != Right)
+            {
+                direction = Left;
+            }
+            break;
+        case KEY_UP:
+            if (direction != Down)
+            {
+                direction = Up;
+            }
+            break;
+        case KEY_DOWN:
+            if (direction != Up)
+            {
+                direction = Down;
+            }
+            break;
+        case KEY_RIGHT:
+            if (direction != Left)
+            {
+                direction = Right;
+            }
+            break;
+        case 'q':
+            exit(0);
+        }
+
+        if (!tail_stop)
+        {
+            move(snake[snake.size() - 1].s_y, snake[snake.size() - 1].s_x);
+            printw(" ");
+            refresh();
+            snake.pop_back();
+        }
+
+        if (direction == Left)
+        {
+            snake.insert(snake.begin(), UnitType(snake[0].s_x - 1, snake[0].s_y));
+        }
+
+        if (direction == Right)
+        {
+            snake.insert(snake.begin(), UnitType(snake[0].s_x + 1, snake[0].s_y));
+        }
+
+        if (direction == Up)
+        {
+            snake.insert(snake.begin(), UnitType(snake[0].s_x, snake[0].s_y - 1));
+        }
+
+        if (direction == Down)
+        {
+            snake.insert(snake.begin(), UnitType(snake[0].s_x, snake[0].s_y + 1));
+        }
+
+        move(snake[0].s_y, snake[0].s_x);
+        addch(snake_char);
+        refresh();
+    }
+
+    void start_game()
+    {
+        while (true)
+        {
+            move_snake();
+            usleep(delay);
+        }
+    }
 };
 
 int main(int argc, char **argv)
 {
     SnakeGame s;
+    s.start_game();
     return 0;
 }
